679ba984f37a2b8a76db40b8d8c070286c216743
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-08-13 10:34:15 +0530

    Add files via upload

---
+++ day_26.py
@@ -0,0 +1,34 @@
+# 26	API
+# 26	Building API
+# 26	HTTP(Hypertext Transfer Protocol)
+# 26	Structure of HTTP
+# 26	Initial Request Line(Status Line)
+# 26	Initial Response Line(Status Line)
+# 26	Header Fields
+# 26	The message body
+# 26	Request Methods
+
+
+# GET: GET method is used to retrieve and get information from the given server using a given URI. Requests using GET should only retrieve data and should have no other effect on the data.
+#
+# POST: POST request is used to create data and send data to the server, for example, creating a new post, file upload, etc. using HTML forms.
+#
+# PUT: Replaces all current representations of the target resource with the uploaded content and we use it modify or update data.
+#
+# DELETE: Removes data
+
+# 26	 Exercises: Day 28
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
