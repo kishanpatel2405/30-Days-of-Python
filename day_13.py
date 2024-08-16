b56b32afe3d7405a81166abbd30f81483a04a674
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-08-16 12:54:55 +0530

    Update day_13.py

--- day_13.py
+++ day_13.py
@@ -19,6 +19,7 @@ odd_number = [i for i in range(10) if i % 2 != 0]
 print(odd_number)
 
 
+
 numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
 positive_even_numbers = [i for i in range(21) if i % 2 == 0 and i > 0]
 print(positive_even_numbers)
